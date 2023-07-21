39f80104aef9124a6d41f3254fcec8397a94c0e7
Tem <temurbakhriddinov@gmail.com>
2023-07-21 18:41:48 -0500

    fix error for build

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,13 +1,12 @@
 /** @format */
 
 'use client';
-import Body from '@/components/shop/body';
 import React from 'react';
 
 const Page = () => {
 	return (
 		<div className='flex flex-col w-full h-full'>
-			<div className='w-full h-full'>body</div>
+			<div className='w-full h-full'>test body</div>
 		</div>
 	);
 };
