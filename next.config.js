98b32ebe05673aac70e4a600f088b7b928b753b4
temur bakhriddinov <temurbakhriddinov@gmail.com>
2023-07-25 03:08:56 -0500

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['m.media-amazon.com', ['i.ibb.co']],
+        domains: ['m.media-amazon.com', 'i.ibb.co'],
         remotePatterns: [
             {
                 protocol: "https",
