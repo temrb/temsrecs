6eef48001dc3461b57bcc992d4f5b6c86f1a00db
Tem <temurbakhriddinov@gmail.com>
2023-08-04 03:06:09 -0500

    blog init

--- src/app/(root)/content/blog/[blog]/page.tsx
+++ src/app/(root)/content/blog/[blog]/page.tsx
@@ -4,6 +4,7 @@ import { PortableText } from '@portabletext/react';
 import Image from 'next/image';
 import { getBlogPost } from '../../../../../../sanity/sanity-utils';
 import ErrorDisplay from '@/components/error-display';
+import { Timer, Calendar } from 'lucide-react';
 
 type Props = {
 	params: {
@@ -12,10 +13,10 @@ type Props = {
 };
 
 export default async function Project({ params }: Props) {
-	const project = await getBlogPost(params.blog);
-	console.log('🚀 ~ file: page.tsx:16 ~ Project ~ project:', project);
+	const post = await getBlogPost(params.blog);
+	console.log('🚀 ~ file: page.tsx:16 ~ Project ~ project:', post);
 
-	if (!project) {
+	if (!post) {
 		return (
 			<div className='pt-6'>
 				<ErrorDisplay errorText='Post not found' />
@@ -24,15 +25,79 @@ export default async function Project({ params }: Props) {
 	}
 
 	return (
-		<div>
-			<header className='flex items-center justify-between'>
-				<h1 className='bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-5xl drop-shadow font-extrabold'>
-					{project?.title}
-				</h1>
-			</header>
+		<div className='p-6 w-full h-full space-y-10'>
+			<section className='flex flex-col space-y-4'>
+				{/* cover image */}
+				<div className='relative w-full h-56'>
+					<Image
+						src={post?.coverImage}
+						fill
+						loading='lazy'
+						style={{ objectFit: 'cover' }}
+						alt={post?.title + ' cover image'}
+						className='p-2 rounded-xl'
+						quality={50}
+						sizes='(max-width: 768px) 100vw, 33vw'
+					/>
+				</div>
 
-			<div className='text-lg text-gray-700 mt-5'>
-				<PortableText value={project?.content} />
+				{/* title content */}
+				<div className='items-center flex lg:flex-row flex-col lg:justify-between justify-center lg:space-y-0 space-y-6'>
+					<div className='flex flex-col justify-center lg:items-start items-center space-y-1'>
+						<h1 className='text-5xl drop-shadow font-extrabold'>
+							{post?.title}
+						</h1>
+						<div className='flex flex-row space-x-2'>
+							{post?.tags?.map((tag: string) => (
+								<p
+									// using sanity tag as key
+									key={tag}
+									className='flex bg-yellow-500/60 p-1 rounded-md shadow-md
+								text-xs items-center justify-center capitalize '
+								>
+									{tag}
+								</p>
+							))}
+						</div>
+					</div>
+					<div className='flex flex-col justify-center space-y-1 lg:items-end items-center'>
+						<span className='flex text-sm items-center font-light justify-center'>
+							<Calendar className='h-4 w-4 mr-1' />
+							<p>
+								{new Date(post?._createdAt).toLocaleDateString('en-US', {
+									month: 'short',
+									day: 'numeric',
+									year: 'numeric',
+								})}
+							</p>
+						</span>
+						<span className='flex text-xs font-light items-center justify-center'>
+							<Timer className='h-4 w-4 mr-1' />
+							<p>{post?.estimatedReadTime} min read</p>
+						</span>
+					</div>
+				</div>
+			</section>
+
+			<div className='text-base'>
+				<PortableText
+					value={post?.content}
+					// components={{
+					// 	types: {
+					// 		BodyImage: (props: any) => (
+					// 			<div className='relative w-full h-96'>
+					// 				<Image
+					// 					src={props?.url}
+					// 					alt={props?.alt}
+					// 					layout='fill'
+					// 					objectFit='cover'
+					// 					className='rounded-xl'
+					// 				/>
+					// 			</div>
+					// 		),
+					// 	},
+					// }}
+				/>
 			</div>
 
 			{/* <Image
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,5 @@
 /** @format */
 
-import Navigation from '@/components/navigation';
-
 export default function Page() {
 	return <div>Page</div>;
 }
