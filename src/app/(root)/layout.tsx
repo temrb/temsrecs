cd86d95ff1fe8d834caa247115c098e220223ddb
Tem <temurbakhriddinov@gmail.com>
2023-08-05 17:01:29 -0500

    update

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -34,15 +34,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 							type='text/javascript'
 							src='https://cdn-cookieyes.com/client_data/5c0d8cc2f5425587bddfde99/script.js'
 						/>
-						<Script type='text/javascript' id='clarity'>
-							{`
-          (function (c, l, a, r, i, t, y) {
-    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments); };
-    t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
-    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
-})(window, document, "clarity", "script", "i2n3gilivs");
-          `}
-						</Script>
 					</>
 				)}
 			</head>
